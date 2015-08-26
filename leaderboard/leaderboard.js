95604f860a7f5c278dbfb97655567e759fbd36ab
Liviu Iancu <lvudesign@gmail.com>
2015-08-27 01:49:52 +0300

    Meteor app update progress

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -3,14 +3,16 @@ PlayersList = new Mongo.Collection('players');
 
 if (Meteor.isClient) {
     Template.leaderboard.helpers({
+
         //show selected player from list
         'showSelectedPlayer': function () {
             var selectedPlayer = Session.get('selectedPlayer');
             return PlayersList.findOne(selectedPlayer);
         },
         'player': function () {
+            var currentUserId = Meteor.userId();
             //get list of players from database
-            return PlayersList.find({}, {
+            return PlayersList.find({createdBy: currentUserId}, {
                 sort: {
                     score: -1,
                     name: 1
@@ -27,6 +29,11 @@ if (Meteor.isClient) {
         }
     });
     Template.leaderboard.events({
+        'click .remove': function () {
+            //get _id from selected player
+            var selectedPlayer = Session.get('selectedPlayer');
+            PlayersList.remove(selectedPlayer);
+        },
         'click .player': function () {
             var playerId = this._id;
             //create temporary SESSION of selected state
@@ -48,6 +55,22 @@ if (Meteor.isClient) {
                 $inc: {
                     score: -5
                 }
+            });
+        }
+    });
+    Template.addPlayerForm.events({
+        'submit form': function () {
+            event.preventDefault();
+            //function userId() returns unique id of currently logged user
+            var currentUserId = Meteor.userId();
+            //get value of input field(playerName) in a variable
+            var playerNameVar = event.target.playerName.value;
+            console.log(playerNameVar);
+            //insert the value from the form (playerNameVar) in the collection PlayersList
+            PlayersList.insert({
+                name: playerNameVar,
+                score: 0,
+                createdBy: currentUserId
             });
         }
     });
